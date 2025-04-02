036cb488b634e71df0cb2eceb0599457faae16e5
Javier Seixas <dev@javierseixas.com>
2025-04-02 09:41:23 +0000

    Export modules

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,28 +1,4 @@
-class Stack {
-    constructor() {
-        this.top = -1;
-        this.items = {};
-    }
-
-    get peek() {
-        return this.items[this.top];
-    }
-
-    push(item) {
-        this.top++;
-        this.items[this.top] = item;
-    }
-
-    pop() {
-        if (this.top === -1) {
-            throw new Error('Stack is empty');
-        }
-        const item = this.items[this.top];
-        delete this.items[this.top];
-        this.top--;
-        return item;
-    }
-}
+const Stack = require('../src/stack')
 
 describe('My Strack', () => {
 
